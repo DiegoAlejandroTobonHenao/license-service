fd13f4471a05870225e4439584487c2d72912bb0
DiegoAlejandroTobonHenao <60477478+DiegoAlejandroTobonHenao@users.noreply.github.com>
2022-10-01 07:04:14 -0500

    Add files via upload

---
+++ servicio-licencias/src/test/java/com/serviciolicencias/ServicioLicenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviciolicencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioLicenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
